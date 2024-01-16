6d93253c9a79a54f923ae78c66e2c2fd067cf31f
dsad2221s <ghksgml0555@naver.com>
2024-01-16 10:29:21 +0900

    findByLoginId, existsByLoginid, findByName

--- src/main/java/com/board/board/repository/MemberRepository.java
+++ src/main/java/com/board/board/repository/MemberRepository.java
@@ -3,6 +3,11 @@ package com.board.board.repository;
 import com.board.board.domain.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
+    Optional<Member> findByLoginId(String loginId);
+    boolean existsByLoginId(String loginId);
+    Optional<Member> findByName(String name);
 }
