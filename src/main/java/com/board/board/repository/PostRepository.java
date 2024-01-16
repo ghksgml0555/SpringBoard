cd30f0000caf41918df3b0255deb850bdf86498d
dsad2221s <ghksgml0555@naver.com>
2024-01-16 23:00:50 +0900

    조회수 구현

--- src/main/java/com/board/board/repository/PostRepository.java
+++ src/main/java/com/board/board/repository/PostRepository.java
@@ -2,6 +2,12 @@ package com.board.board.repository;
 
 import com.board.board.domain.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    @Modifying
+    @Query("update Post p set p.view = p.view+1 where p.id = :id")
+    int updateView(Long id);
 }
