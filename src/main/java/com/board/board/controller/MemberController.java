4fcadb2edd2044098348d0f6e146374b898cef0c
dsad2221s <ghksgml0555@naver.com>
2024-01-16 10:26:07 +0900

    회원가입 구현

---
+++ src/main/java/com/board/board/controller/MemberController.java
@@ -0,0 +1,43 @@
+package com.board.board.controller;
+
+
+import com.board.board.dto.MemberDto;
+import com.board.board.service.MemberService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+@Slf4j
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model){
+        model.addAttribute("memberDto", new MemberDto());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("members/new")
+    public String join(@ModelAttribute @Valid MemberDto memberDto,
+                       BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "members/createMemberForm";
+        }
+        memberService.join(memberDto);
+        return "redirect:/";
+    }
+}
